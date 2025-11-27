29bc9737780e4ba0779527ed70578add9114874c
whosam <hossam.samy.rashed@gmail.com>
2025-11-27 17:52:05 +0200

    docerized the app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
-    open: true
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
-
